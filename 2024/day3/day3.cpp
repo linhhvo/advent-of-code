2fcc064e9aa7318229b96e829871ea3980be4ed5
elly <lhvo2112@gmail.com>
2025-07-12 15:06:54 +0300

    update part1 using stringstream

--- 2024/day3/day3.cpp
+++ 2024/day3/day3.cpp
@@ -5,16 +5,9 @@
 
 using namespace std;
 
-int main() {
-
-  ifstream inputFile{"input.txt"};
-
-  if (!inputFile) {
-    cerr << "Can't open input file.\n";
-    return 1;
-  }
-
-  string inputStr{};
+// calculate the sum of multiplications that match mul(X,Y) instruction in a
+// given string input
+int calculateSum(string inputStr) {
   int sum{0};
 
   regex mulPattern{"mul\\((\\d+),(\\d+)\\)"};
@@ -29,21 +22,38 @@ int main() {
 
   smatch matchedResults;
 
-  while (getline(inputFile, inputStr)) {
-    while (regex_search(inputStr, matchedResults, mulPattern)) {
-      // extract numbers from regex capturing groups and convert to integers
-      int num1{stoi(matchedResults[1].str())};
-      int num2{stoi(matchedResults[2].str())};
+  while (regex_search(inputStr, matchedResults, mulPattern)) {
+    // extract numbers from regex capturing groups and convert to integers
+    int num1{stoi(matchedResults[1].str())};
+    int num2{stoi(matchedResults[2].str())};
 
-      sum = sum + (num1 * num2);
+    sum = sum + (num1 * num2);
 
-      // replace input string with the substring follows the end of the matched
-      // result
-      inputStr = matchedResults.suffix().str();
-    }
+    // replace input string with the substring follows the end of the matched
+    // result
+    inputStr = matchedResults.suffix().str();
   }
 
-  cout << "multiply result: " << sum << '\n';
+  return sum;
+}
+
+int main() {
+
+  ifstream inputFile{"input.txt"};
+
+  if (!inputFile) {
+    cerr << "Can't open input file.\n";
+    return 1;
+  }
+
+  stringstream ss;
+  int finalSum{0};
+
+  ss << inputFile.rdbuf();
+
+  finalSum += calculateSum(ss.str());
+
+  cout << "multiply result: " << finalSum << '\n';
 
   return 0;
 }
