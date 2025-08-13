76013d8b15ffd5ea9b22f91669d8dde78988c8f7
elly <lhvo2112@gmail.com>
2025-08-13 13:06:00 +0300

    added horizontal check

--- 2024/day4/day4.cpp
+++ 2024/day4/day4.cpp
@@ -7,25 +7,29 @@ using namespace std;
 
 int isXmasVertical(int rowIdx, int colIdx, vector<vector<char>> input2d) {
   int isXmas = 0;
+
+  // check both up and down directions
   if (rowIdx > 2 && rowIdx < static_cast<int>(input2d.size()) - 3) {
-    cout << "checking X\n";
     if (input2d[rowIdx + 1][colIdx] == 'M' &&
         input2d[rowIdx + 2][colIdx] == 'A' &&
         input2d[rowIdx + 3][colIdx] == 'S') {
       isXmas = 1;
-      cout << "xmas matched\n";
     } else if (input2d[rowIdx - 1][colIdx] == 'M' &&
                input2d[rowIdx - 2][colIdx] == 'A' &&
                input2d[rowIdx - 3][colIdx] == 'S') {
       isXmas = 1;
     }
-  } else if (rowIdx < 2) {
+  }
+  // only check down direction
+  else if (rowIdx < 3) {
     if (input2d[rowIdx + 1][colIdx] == 'M' &&
         input2d[rowIdx + 2][colIdx] == 'A' &&
         input2d[rowIdx + 3][colIdx] == 'S') {
       isXmas = 1;
     }
-  } else {
+  }
+  // only check up direction
+  else {
     if (input2d[rowIdx - 1][colIdx] == 'M' &&
         input2d[rowIdx - 2][colIdx] == 'A' &&
         input2d[rowIdx - 3][colIdx] == 'S') {
@@ -34,6 +38,47 @@ int isXmasVertical(int rowIdx, int colIdx, vector<vector<char>> input2d) {
   }
 
   return isXmas;
+}
+
+int isXmasHorizontal(int rowIdx, int colIdx, vector<vector<char>> input2d) {
+  int isXmas = 0;
+  vector<char> currentRow{input2d[rowIdx]};
+
+  // check both left and right directions
+  if (colIdx > 2 && colIdx < static_cast<int>(currentRow.size()) - 3) {
+    if (currentRow[colIdx + 1] == 'M' && currentRow[colIdx + 2] == 'A' &&
+        currentRow[colIdx + 3] == 'S') {
+      isXmas = 1;
+    } else if (currentRow[colIdx - 1] == 'M' && currentRow[colIdx - 2] == 'A' &&
+               currentRow[colIdx - 3] == 'S') {
+      isXmas = 1;
+    }
+  }
+  // check only right direction
+  else if (colIdx < 3) {
+    if (currentRow[colIdx + 1] == 'M' && currentRow[colIdx + 2] == 'A' &&
+        currentRow[colIdx + 3] == 'S') {
+      isXmas = 1;
+    }
+  }
+  // check only left direction
+  else {
+    if (currentRow[colIdx - 1] == 'M' && currentRow[colIdx - 2] == 'A' &&
+        currentRow[colIdx - 3] == 'S') {
+      isXmas = 1;
+    }
+  }
+
+  return isXmas;
+}
+
+int isXmasFound(int rowIdx, int colIdx, vector<vector<char>> input2d) {
+  if (isXmasVertical(rowIdx, colIdx, input2d) ||
+      isXmasHorizontal(rowIdx, colIdx, input2d)) {
+    return 1;
+  }
+
+  return 0;
 }
 
 int main() {
@@ -62,8 +107,8 @@ int main() {
   for (size_t i{0}; i < allInput.size(); ++i) {
     for (size_t j{0}; j < allInput[i].size(); ++j) {
       if (allInput[i][j] == 'X') {
-        cout << "X found\n";
-        if (isXmasVertical(i, j, allInput) == 1) {
+        cout << "X found at (" << i << ", " << j << ")\n";
+        if (isXmasFound(i, j, allInput) == 1) {
           count++;
           cout << "xmas found\n";
         };
