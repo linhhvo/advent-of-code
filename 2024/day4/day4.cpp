eb3f72e862f2af3130b50e5c5178472d6326e7c1
elly <lhvo2112@gmail.com>
2025-08-21 19:15:15 +0300

    finished day4 part1

--- 2024/day4/day4.cpp
+++ 2024/day4/day4.cpp
@@ -5,19 +5,21 @@
 
 using namespace std;
 
-int isXmasVertical(int rowIdx, int colIdx, vector<vector<char>> input2d) {
-  int isXmas = 0;
+int verticalCount(int rowIdx, int colIdx, vector<vector<char>> input2d) {
+  int xmasCount = 0;
 
   // check both up and down directions
   if (rowIdx > 2 && rowIdx < static_cast<int>(input2d.size()) - 3) {
     if (input2d[rowIdx + 1][colIdx] == 'M' &&
         input2d[rowIdx + 2][colIdx] == 'A' &&
         input2d[rowIdx + 3][colIdx] == 'S') {
-      isXmas = 1;
-    } else if (input2d[rowIdx - 1][colIdx] == 'M' &&
-               input2d[rowIdx - 2][colIdx] == 'A' &&
-               input2d[rowIdx - 3][colIdx] == 'S') {
-      isXmas = 1;
+      xmasCount++;
+    }
+
+    if (input2d[rowIdx - 1][colIdx] == 'M' &&
+        input2d[rowIdx - 2][colIdx] == 'A' &&
+        input2d[rowIdx - 3][colIdx] == 'S') {
+      xmasCount++;
     }
   }
   // only check down direction
@@ -25,7 +27,7 @@ int isXmasVertical(int rowIdx, int colIdx, vector<vector<char>> input2d) {
     if (input2d[rowIdx + 1][colIdx] == 'M' &&
         input2d[rowIdx + 2][colIdx] == 'A' &&
         input2d[rowIdx + 3][colIdx] == 'S') {
-      isXmas = 1;
+      xmasCount++;
     }
   }
   // only check up direction
@@ -33,56 +35,135 @@ int isXmasVertical(int rowIdx, int colIdx, vector<vector<char>> input2d) {
     if (input2d[rowIdx - 1][colIdx] == 'M' &&
         input2d[rowIdx - 2][colIdx] == 'A' &&
         input2d[rowIdx - 3][colIdx] == 'S') {
-      isXmas = 1;
+      xmasCount++;
     }
   }
 
-  return isXmas;
+  return xmasCount;
 }
 
-int isXmasHorizontal(int rowIdx, int colIdx, vector<vector<char>> input2d) {
-  int isXmas = 0;
+int horizontalCount(int rowIdx, int colIdx, vector<vector<char>> input2d) {
+  int xmasCount = 0;
   vector<char> currentRow{input2d[rowIdx]};
 
   // check both left and right directions
   if (colIdx > 2 && colIdx < static_cast<int>(currentRow.size()) - 3) {
     if (currentRow[colIdx + 1] == 'M' && currentRow[colIdx + 2] == 'A' &&
         currentRow[colIdx + 3] == 'S') {
-      isXmas = 1;
-    } else if (currentRow[colIdx - 1] == 'M' && currentRow[colIdx - 2] == 'A' &&
-               currentRow[colIdx - 3] == 'S') {
-      isXmas = 1;
+      xmasCount++;
+    }
+
+    if (currentRow[colIdx - 1] == 'M' && currentRow[colIdx - 2] == 'A' &&
+        currentRow[colIdx - 3] == 'S') {
+      xmasCount++;
     }
   }
   // check only right direction
   else if (colIdx < 3) {
     if (currentRow[colIdx + 1] == 'M' && currentRow[colIdx + 2] == 'A' &&
         currentRow[colIdx + 3] == 'S') {
-      isXmas = 1;
+      xmasCount++;
     }
   }
   // check only left direction
   else {
     if (currentRow[colIdx - 1] == 'M' && currentRow[colIdx - 2] == 'A' &&
         currentRow[colIdx - 3] == 'S') {
-      isXmas = 1;
+      xmasCount++;
     }
   }
 
-  return isXmas;
+  return xmasCount;
 }
 
-int isXmasFound(int rowIdx, int colIdx, vector<vector<char>> input2d) {
-  if (isXmasVertical(rowIdx, colIdx, input2d) ||
-      isXmasHorizontal(rowIdx, colIdx, input2d)) {
-    return 1;
-  }
+int diagonalDownCount(int rowIdx, int colIdx, vector<vector<char>> input2d) {
+  int xmasCount = 0;
+  vector<char> currentRow{input2d[rowIdx]};
 
-  return 0;
+  if (rowIdx < static_cast<int>(input2d.size()) - 3) {
+    // check both down diagnol left and right directions
+    if (colIdx > 2 && colIdx < static_cast<int>(currentRow.size()) - 3) {
+      if (input2d[rowIdx + 1][colIdx + 1] == 'M' &&
+          input2d[rowIdx + 2][colIdx + 2] == 'A' &&
+          input2d[rowIdx + 3][colIdx + 3] == 'S') {
+        xmasCount++;
+      }
+
+      if (input2d[rowIdx + 1][colIdx - 1] == 'M' &&
+          input2d[rowIdx + 2][colIdx - 2] == 'A' &&
+          input2d[rowIdx + 3][colIdx - 3] == 'S') {
+        xmasCount++;
+      }
+    }
+    // check down diagnol only right direction
+    else if (colIdx < 3) {
+      if (input2d[rowIdx + 1][colIdx + 1] == 'M' &&
+          input2d[rowIdx + 2][colIdx + 2] == 'A' &&
+          input2d[rowIdx + 3][colIdx + 3] == 'S') {
+        xmasCount++;
+      }
+    }
+    // check down diagnol only left direction
+    else {
+      if (input2d[rowIdx + 1][colIdx - 1] == 'M' &&
+          input2d[rowIdx + 2][colIdx - 2] == 'A' &&
+          input2d[rowIdx + 3][colIdx - 3] == 'S') {
+        xmasCount++;
+      }
+    }
+  }
+  return xmasCount;
+}
+
+int diagonalUpCount(int rowIdx, int colIdx, vector<vector<char>> input2d) {
+  int xmasCount = 0;
+  vector<char> currentRow{input2d[rowIdx]};
+
+  if (rowIdx > 2) {
+    // check both up diagnol left and right directions
+    if (colIdx > 2 && colIdx < static_cast<int>(currentRow.size()) - 3) {
+      if (input2d[rowIdx - 1][colIdx - 1] == 'M' &&
+          input2d[rowIdx - 2][colIdx - 2] == 'A' &&
+          input2d[rowIdx - 3][colIdx - 3] == 'S') {
+        xmasCount++;
+      }
+
+      if (input2d[rowIdx - 1][colIdx + 1] == 'M' &&
+          input2d[rowIdx - 2][colIdx + 2] == 'A' &&
+          input2d[rowIdx - 3][colIdx + 3] == 'S') {
+        xmasCount++;
+      }
+    }
+    // check up diagnol only right direction
+    else if (colIdx < 3) {
+      if (input2d[rowIdx - 1][colIdx + 1] == 'M' &&
+          input2d[rowIdx - 2][colIdx + 2] == 'A' &&
+          input2d[rowIdx - 3][colIdx + 3] == 'S') {
+        xmasCount++;
+      }
+    }
+    // check up diagnol only left direction
+    else {
+      if (input2d[rowIdx - 1][colIdx - 1] == 'M' &&
+          input2d[rowIdx - 2][colIdx - 2] == 'A' &&
+          input2d[rowIdx - 3][colIdx - 3] == 'S') {
+        xmasCount++;
+      }
+    }
+  }
+  return xmasCount;
+}
+
+int xmasFoundCount(int rowIdx, int colIdx, vector<vector<char>> input2d) {
+  return (verticalCount(rowIdx, colIdx, input2d) +
+          horizontalCount(rowIdx, colIdx, input2d) +
+          diagonalDownCount(rowIdx, colIdx, input2d) +
+          diagonalUpCount(rowIdx, colIdx, input2d));
 }
 
 int main() {
-  ifstream inputFile{"test.txt"};
+  // ifstream inputFile{"test.txt"};
+  ifstream inputFile{"input.txt"};
 
   if (!inputFile) {
     cerr << "Can't open input file.\n";
@@ -95,11 +176,11 @@ int main() {
 
   // store input in a 2D vector
   while (getline(inputFile, tempLine)) {
-    vector<char> lineInput;
+    vector<char> lineChar;
     for (char c : tempLine) {
-      lineInput.push_back(c);
+      lineChar.push_back(c);
     }
-    allInput.push_back(lineInput);
+    allInput.push_back(lineChar);
   }
 
   int count{0};
@@ -108,18 +189,17 @@ int main() {
     for (size_t j{0}; j < allInput[i].size(); ++j) {
       if (allInput[i][j] == 'X') {
         cout << "X found at (" << i << ", " << j << ")\n";
-        if (isXmasFound(i, j, allInput) == 1) {
-          count++;
-          cout << "xmas found\n";
+        int numOfXmas{xmasFoundCount(i, j, allInput)};
+        if (numOfXmas > 0) {
+          count = count + numOfXmas;
+          cout << numOfXmas << " xmas found\n";
         };
       }
-      // cout << allInput[i][j] << ", ";
     }
-    // cout << '\n';
   }
 
-  cout << count;
-  cout << '\n';
+  cout << "\nnum of xmas: " << count;
+  cout << "\n\n";
 
   return 0;
 }
