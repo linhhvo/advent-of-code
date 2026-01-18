22f1f47b32132468e4ca06f52fecc10c916c5504
elly <lhvo2112@gmail.com>
2026-01-18 19:22:59 +0200

    done day5 part2

---
+++ 2025/day5/part2.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bufio"
+	"cmp"
+	"fmt"
+	"log"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+// convert ID range limits to integers
+func getRange(line string) (int, int) {
+	splitLine := strings.Split(line, "-")
+
+	lower, err := strconv.Atoi(splitLine[0])
+
+	if err != nil {
+		log.Fatalf("can't convert lower to number")
+	}
+
+	upper, err := strconv.Atoi(splitLine[1])
+
+	if err != nil {
+		log.Fatalf("can't convert upper to number")
+	}
+
+	return lower, upper
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	// file, err := os.Open("sample.txt")
+
+	if err != nil {
+		log.Fatalf("can't open file: %s", err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var ranges []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// end loop when reaches empty line
+		if line == "" {
+			break
+		}
+
+		ranges = append(ranges, line)
+	}
+
+	// sort ranges in ascending order based on lower limit
+	slices.SortFunc(ranges, func(range1, range2 string) int {
+		lower1, _ := getRange(range1)
+		lower2, _ := getRange(range2)
+
+		return cmp.Compare(lower1, lower2)
+	})
+
+	count := 0
+	upperMax := 0
+
+	type limit struct {
+		lower int
+		upper int
+	}
+
+	var groupings [][]limit
+	var overlapGroup []limit
+
+	for i := range ranges {
+		lower, upper := getRange(ranges[i])
+
+		// separate groups of ranges that overlap with each other
+		if lower > upperMax {
+			if len(overlapGroup) > 0 {
+				groupings = append(groupings, overlapGroup)
+			}
+			overlapGroup = []limit{}
+		}
+
+		overlapGroup = append(overlapGroup, limit{lower, upper})
+		upperMax = upper
+	}
+
+	// add the last group if there is no more ID range to read
+	if len(overlapGroup) > 0 {
+		groupings = append(groupings, overlapGroup)
+	}
+
+	for i := range groupings {
+		rangeCount := 0
+		// if there is no overlap range
+		if len(groupings[i]) == 1 {
+			rangeCount = groupings[i][0].upper - groupings[i][0].lower + 1
+		} else { // if there are overlap ranges
+			maxLimit := slices.MaxFunc(groupings[i], func(a, b limit) int {
+				return cmp.Compare(a.upper, b.upper)
+			})
+
+			rangeCount = maxLimit.upper - groupings[i][0].lower + 1
+		}
+
+		count = count + rangeCount
+	}
+
+	fmt.Println("result:", count)
+
+}
